c59fc1dbff65cc5d06ddadd61f269a69a1f22155
murata <kazunari.loto6@gmail.com>
2022-06-17 02:23:23 +0000

    addlayout

--- resources/views/butter.blade.php
+++ resources/views/butter.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
+@section('title', '打者一覧')
 
-        <title>打者一覧</title>
-    </head>
-    <body>
+@section('content')
         <h1>打者一覧</h1>
-    </body>
-</html>
+@endsection
