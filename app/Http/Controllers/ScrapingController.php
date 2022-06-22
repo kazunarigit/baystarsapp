d4758009e6c49774eb971d9e21d6456b4752faaa
murata <kazunari.loto6@gmail.com>
2022-06-22 05:08:36 +0000

    comment and db操作

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -11,47 +11,47 @@ use App\Models\Playerdata;
 class ScrapingController extends Controller
 {
     public function pitcher()
-    {
+    {　　// ここからスクレイピングでデータを持ってくる。
         $client = new Client();
         $crawler = $client->request('GET', 'https://baseball.yahoo.co.jp/npb/teams/3/memberlist?kind=p');
         $info = $crawler->filter('.bb-playerTable__row')->each(function ($tr) {
         $data = $tr->text();
       
-        return $data;
-        });
+        return $data; // ここまで
+        });　//選手データ分繰り返し
         foreach($info as $data) {
             
-            // $playerdata = new Playerdata;
-            // $playerdata->playername = $data[1];
+            // データを分割
             $playerdata = explode(" ", $data);
             // print_r($playerdata);
             dump($playerdata);
             
-            $playerdata = Playerdata::select('playername', 'ining', 'balls', 'hit_by_a_pitch', 'by_homeruns', 'wins', 'loses', 'saves', 'resp_points', 'lost_points', 'saved_adv')->get();
+            // $playerdata = Playerdata::select('playername', 'ining', 'balls', 'hit_by_a_pitch', 'by_homeruns', 'wins', 'loses', 'saves', 'resp_points', 'lost_points', 'saved_adv')->get();
             // dd($playerdata);
-            // $playerdata->save();
+            $playerdata->save();
         };
         return redirect('/pitcherinfo');
     }    
     
     
      public function butter()
-     {
+     {　// ここからスクレイピングでデータを持ってくる。
         $client = new Client();
         $crawler = $client->request('GET', 'https://baseball.yahoo.co.jp/npb/teams/3/memberlist?kind=b');
         $info = $crawler->filter('.bb-playerTable__row')->each(function ($tr) {
         $data = $tr->text();
       
-        return $data;
-        });
+        return $data; // ここまで
+        }); //選手データ分繰り返し
         foreach($info as $data) {
+            // データを分割
             $playerdata = explode(" ", $data);
             //print_r($playerdata) ;
             dump($playerdata);
             
-            $playerdata = Playerdata::select('playername', 'times_at_but', 'hit', 'hit_point', 'hit_adv')->get();
+            // $playerdata = Playerdata::select('playername', 'times_at_but', 'hit', 'hit_point', 'hit_adv')->get();
             
-            // $playerdata->save();
+            $playerdata->save();
             
         };
         return redirect('/butterinfo');
