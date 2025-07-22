e9dad711dec1e6fc6fbee096ef4da538c2a03e41
kazunarigit <63156415+kazunarigit@users.noreply.github.com>
2025-07-22 09:44:19 +0900

    Update ScrapingController.php

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -11,76 +11,7 @@ use App\Models\Playerdata2;
 
 class ScrapingController extends Controller
 {
-    // public function scraping(Request $request)
-    // {   // ここからスクレイピングデータを持ってくる。
-    //     $client = new Client();
-    //     $crawler = $client->request('GET', 'https://baseball.yahoo.co.jp/npb/teams/3/memberlist?kind=b');
-    //     $info = $crawler->filter('.bb-playerTable__row')->each(function ($tr) {
-    //         $tdData = $tr->filter('.bb-playerTable__data')->each(function ($td) {
-    //             return $td->text();
-    //         });
-    //         return $tdData;
-    //     });//1か月ごとから試合分の結果を配列で取得～選手のデータを取得
-    //      
-    //                 }
-                    
-    //             // playerdataのインスタンスを生成し、データベースのテーブルに保存
-            
-    //             $playerdata1 = new Playerdata();
-    //             $playerdata1->lastname = $lastName;
-    //             $playerdata1->firstname = $firstName;
-    //             $playerdata1->times_at_but = $data[5];
-    //             $playerdata1->hit = $data[7];
-    //             $playerdata1->hit_point = $data[12];
-    //             $playerdata1->hit_adv = (int)$data[3] / 100;
-    //             $playerdata1->homeruns = $data[10];
-    //             $playerdata1->steals = $data[19];
-    //             $playerdata1->games = $data[4];
-    //             $playerdata1->box = $data[6];
-    //              // if($data[1] == "オースティン") {
-    //              //         dd($data,$playerdata);
-    //              //     }
-            
-    //             $playerdata1->save();
-    //         }    
     
-    //         return redirect('/pitcherinfo');
-        
-    //     //  public function butterscraping()
-    //  // ここからスクレイピングでデータを持ってくる。
-    //     $client = new Client();
-    //     // 月ごとの試合のURLを書く。https://baseball.yahoo.co.jp/npb/teams/3/schedule?month=2022-04
-    //     $crawler = $client->request('GET', 'https://baseball.yahoo.co.jp/npb/teams/3/memberlist?kind=p');
-    //     // ページの試合結果に飛ぶタグの指定　bb-calendarTable__status
-    //     $info = $crawler->filter('.bb-playerTable__row')->each(function ($tr) {
-    //         $tdData = $tr->filter('.bb-playerTable__data')->each(function ($td) {
-    //           return $td->text(); 
-    //         });
-    //         return $tdData();
-    //     });
-    //     
-            
-               
-    //             // playerdataのインスタンスを生成し、データベースのテーブルに保存
-    //             $playerdata2 = new Playerdata2();
-    //             $playerdata2->lastname = $lastName;
-    //             $playerdata2->firstname = $firstName;
-    //             $playerdata2->ining = $data[4];
-    //             $playerdata2->hit_by_a_pitch = $data[16];
-    //             $playerdata2->by_homeruns = $data[17];
-    //             $playerdata2->wins = $data[9];
-    //             $playerdata2->loses = $data[10];
-    //             $playerdata2->saves = $data[13];
-    //             $playerdata2->resp_points = $data[25];
-    //             $playerdata2->lost_points = $data[24];
-    //             $playerdata2->saved_adv = (int)$data[3] / 100;
-                
-    //             $playerdata2->save();
-    //         }    
-    
-    //         return redirect('/butterinfo');
-    // }
-
     public function scraping(Request $request)
     {
         // 月ごとの試合結果の一覧ページのURLを書く　https://baseball.yahoo.co.jp/npb/teams/3/schedule?month=2022-04
